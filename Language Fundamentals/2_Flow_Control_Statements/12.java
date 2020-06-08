5d15e5d56a04164b18fc6c5a76ea04cfab6e93d9
Anupam Haldkar <ahdev2020@outlook.com>
2020-06-08 14:44:36 +0530

    Flow Control_2

---
+++ Language Fundamentals/2_Flow_Control_Statements/12.java
@@ -0,0 +1,11 @@
+/*Write a program to print even numbers between 23 and 57. Each number should be printed in a separate row.
+Solution:
+*/
+class Main {
+	public static void main(String[] args) {
+		for (int i=24;i<58 ;i=i+2 )
+ {
+			System.out.println(i);
+		}
+	}
+}
