49f82f097f15f46fc2a62a3d3b1747772af2ba72
Anupam Haldkar <ahdev2020@outlook.com>
2020-06-24 23:41:54 +0530

    List

---
+++ 5_Collection/1_List/1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Month {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("Jan");
+		list.add("Feb");
+		list.add("Mar");
+		list.add("Apr");
+		list.add("May");
+		list.add("Jun");
+		list.add("Jul");
+		list.add("Aug");
+		list.add("Sep");
+		list.add("Oct");
+		list.add("Nov");
+		list.add("Dec");
+		for(String a : list){
+			System.out.println(a);
+		}
+
+	}
+}
+
+
