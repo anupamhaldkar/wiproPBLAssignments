c730d2db5f1c40ff877878999e099fa8e98bc3cc
Anupam Haldkar <ahdev2020@outlook.com>
2020-06-26 23:48:42 +0530

    List More Q.

---
+++ 5_Collection/1_List/5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class MonthL {
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<>();
+		list.add("Jan");
+		list.add("Feb");
+		list.add("Mar");
+		list.add("Apr");
+		list.add("May");
+		list.add("Jun");
+		list.add("Jul");
+		list.add("Aug");
+		list.add("Sep");
+		list.add("Oct");
+		list.add("Nov");
+		list.add("Dec");
+		for(String a : list){
+			System.out.println(a);
+		}
+
+	}
+}
