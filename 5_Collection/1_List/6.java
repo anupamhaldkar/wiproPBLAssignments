4e3d9e37a65bbdbe7eb8b91efbbe3e2180aa5f37
Anupam Haldkar <ahdev2020@outlook.com>
2020-06-28 23:50:19 +0530

    List Q.

---
+++ 5_Collection/1_List/6.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class MonthV {
+	public static void main(String[] args) {
+		Vector<String> list = new Vector<>();
+		list.add("Jan");
+		list.add("Feb");
+		list.add("Mar");
+		list.add("Apr");
+		list.add("May");
+		list.add("Jun");
+		list.add("Jul");
+		list.add("Aug");
+		list.add("Sep");
+		list.add("Oct");
+		list.add("Nov");
+		list.add("Dec");
+		for(String a : list){
+			System.out.println(a);
+		}
+
+	}
+}
