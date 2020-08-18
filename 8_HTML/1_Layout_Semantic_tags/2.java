f3602c803c7f09059265d5bfc887ea4d7468afa3
Anupam Haldkar <ahdev2020@outlook.com>
2020-08-18 14:02:42 +0530

    Html Layout more

---
+++ 8_HTML/1_Layout_Semantic_tags/2.java
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Assignment-2</title>
+    </head>
+    <body>
+        <p>Ordered List</p>
+        <ol>
+            <li>Google</li>
+            <li>Microsoft</li>
+            <li>Apple</li>
+            <li>Adobe</li>
+        </ol>
+        Unordered List
+        <ul>
+            <li>India</li>
+            <li>United States</li>
+            <li>Russia</li>
+            <li>Britain</li>
+        </ul>
+    </body>
+</html>
