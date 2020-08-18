bc1dfd9fe8b5c501de90a532d50484951e063d04
Anupam Haldkar <ahdev2020@outlook.com>
2020-08-18 14:00:43 +0530

    Html layout tags

---
+++ 8_HTML/1_Layout_Semantic_tags/1.java
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>1st Assignment</title>
+    </head>
+    <body>
+        <h1>A</h1>
+        <h2>n</h2>
+        <h3>u</h3>
+        <h4>p</h4>
+        <h5>a</h5>
+        <h6>m</h6>
+
+        <h1>H</h1>
+        <h2>a</h2>
+        <h3>l</h3>
+        <h4>d</h4>
+        <h5>k</h5>
+        <h6>a</h6>
+        <h1>r</h1>
+        
+    </body>
+</html>
