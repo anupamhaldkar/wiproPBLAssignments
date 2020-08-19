ea6dfe00f5d4cf02977516a8248789c532f162fd
Anupam Haldkar <ahdev2020@outlook.com>
2020-08-19 14:04:26 +0530

    Html Semantic

---
+++ 8_HTML/1_Layout_Semantic_tags/4.java
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Assignment-4</title>
+    </head>
+    <body>
+        <img  src="https://github.com/anupamhaldkar/Covid-19Tracker-StaySafe/raw/master/ss1.png" width="200px" height="200px" border="2">
+    </body>
+</html>
