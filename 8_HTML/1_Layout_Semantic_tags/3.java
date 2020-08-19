0fd0901318ec18f12e470c3350cb2cc6bd8b223c
Anupam Haldkar <ahdev2020@outlook.com>
2020-08-19 14:03:37 +0530

    Html Layout more

---
+++ 8_HTML/1_Layout_Semantic_tags/3.java
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <title>Assignment-3</title>
+    </head>
+    <body>
+        <p>List 1</p>
+        <ol>
+            <li>One
+            <ol type="a">
+                <li>Uno</li>
+                <li>Hanna</li>
+            </ol>
+            </li>
+        <li>Two
+        <ol type="a">
+            <li>Dos</li>
+            <li>Dool</li>
+        </ol>
+        </li>
+        <li>Three
+        <ol type="a">
+            <li>Tres</li>
+            <li>Set</li>
+        </ol>
+        </li>
+        </ol>
+
+        <p>List 2</p>
+        <ul>
+            <li>Chevy
+            <ul type="circle">
+                <li>Cavilier</li>
+                <li>Malibu</li>
+            </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>Ford
+            <ul type="circle">
+                <li>Escort</li>
+                <li>F100</li>
+            </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>Mazda
+            <ul type="circle">
+                <li>626</li>
+            </ul>
+            </li>
+        </ul>
+        <p>List 3</p>
+        <ol type="A">
+            <li>Fruits
+                <ol type="i">
+                    <li>Apples, Oranges, Pears, bananas</li>
+                </ol>
+            </li>
+            <li>Vegetables
+                <ol type="i">
+                    <li>Corn
+                        <ol type="a">
+                            <li>on the cob</li>
+                            <li>cream</li>
+                        </ol>
+                    </li>
+                    <li>Carrots</li>
+                    <li>Green Beans</li>
+                </ol>
+            </li>
+        </ol>
+    </body>
+</html>
