807d3a1e38450bf6343e290d88a04089ee1cace4
Anupam Haldkar <ahdev2020@outlook.com>
2020-08-22 14:05:43 +0530

    Html Semantic more

---
+++ 8_HTML/1_Layout_Semantic_tags/5.java
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Assignment-5</title>
+    </head>
+    <body>
+        <a id="top" href="#bottom" style="position: fixed; top: auto; left: 700;" >Click to go bottom</a>
+
+        <a id="bottom" href="#top" style="position: fixed; left: 700; bottom: 10; "> Click to go Top</a>
+    </body>
+</html>
