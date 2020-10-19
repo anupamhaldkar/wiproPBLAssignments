a9c4354d13098c876ff015646887395098219393
Anupam Haldkar <ahdev2020@outlook.com>
2020-10-19 23:52:34 +0530

    First Servlet

---
+++ 10_Servlet_JSP/FirstServlet/Servlet.java
@@ -0,0 +1,30 @@
+package FirstServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class First
+ */
+@WebServlet("/Welcome")
+public class First extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        //It returns  a PrintWriter object that can send character text to the client
+        pw.print("<html><body><h1>My first Servlet Program</h1></body></html>");
+    }
+
+}
