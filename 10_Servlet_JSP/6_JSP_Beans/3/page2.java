89c9de2fc92c880bc06ec5cb3e355c82dbb1824b
Anupam Haldkar <ahdev2020@outlook.com>
2020-10-26 00:50:14 +0530

    JSP Full stack 🌕

---
+++ 10_Servlet_JSP/6_JSP_Beans/3/page2.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+public class Page2 implements Serializable{
+    private int num;
+    public void setNum(int num) {
+        this.num=num;
+    }
+    
+    public int getNum() {
+        return num;
+    }
+}
+
